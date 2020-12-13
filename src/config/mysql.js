f62190d2efa8828b6d52ddf069bb94a42f4759e4
Andrawan Erlang Pradana <73692809+andrawanerlang1@users.noreply.github.com>
2020-12-14 02:30:29 +0700

    Add files via upload

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql')
+require('dotenv').config()
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: process.env.host,
+  user: process.env.user,
   password: '',
   database: 'coffee_shop',
   timezone: 'UTC'
